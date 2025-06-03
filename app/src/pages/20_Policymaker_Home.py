10974d4c30de2cc43569d4809ce5bf68ab91f9d7
Katherine Ahn <ahn.ka@northeastern.edu>
2025-06-03 22:23:26 +0200

    updated customize move

--- app/src/pages/20_Policymaker_Home.py
+++ app/src/pages/20_Policymaker_Home.py
@@ -14,7 +14,7 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('See Scores Over Time in a Regression', 
+if st.button('See Features Over Time', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/21_Regression.py')
