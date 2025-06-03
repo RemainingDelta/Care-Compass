d22de479aacc8f29c50aaf9ad3faf2546f14076a
Katherine Ahn <ahn.ka@northeastern.edu>
2025-06-04 01:17:42 +0200

    customize move widgets blueprint

--- app/src/pages/01_Customize_Move.py
+++ app/src/pages/01_Customize_Move.py
@@ -16,69 +16,97 @@ SideBarLinks()
 st.header('Customize Your Move!')
 
 # You can access the session state to make a more customized/personalized app experience
-st.write(f"### Hi, {st.session_state['first_name']}.")
+st.write(f"### Hi, {st.session_state['first_name']}. Rank the factors in order of priority -- 1 being the highest and 6 the lowest.")
+st.write("")
+st.write("")
 
-col1, col2, col3 = st.columns(3)
 options = ["Prevention","Health System","Rapid Response","Detection & Reporting", 
            "International Norms Compliance","Risk Environment"]
 
-col1.subheader("Rank the factors in order of priority (1 being the highest and 6 the lowest)")
+#EX DATAFRAME
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+
+col1, col2 = st.columns(2)
+
 with col1:
-    option1 = st.selectbox(
-        "1:",
-        options,
-        index=None,
-        placeholder="Select PRIORITY healthcare factor...",
-    )
-    option2 = st.selectbox(
-        "2:",
-        options,
-        index=None,
-        placeholder="Select SECONDARY healthcare factor...",
-    )
-    option3 = st.selectbox(
-        "3:",
-        options,
-        index=None,
-        placeholder="Select TERTIARY healthcare factor...",
-    )
+    col1a, col1b = st.columns(2)
+    
+    with col1a: 
+        option1 = st.selectbox(
+            "1:",
+            options,
+            index=None,
+            placeholder="Select PRIORITY healthcare factor...",
+        )
+        st.write("\n")
+
+    
+        option2 = st.selectbox(
+            "2:",
+            options,
+            index=None,
+            placeholder="Select SECONDARY healthcare factor...",
+        )
+        st.write("\n")
+
+        option3 = st.selectbox(
+            "3:",
+            options,
+            index=None,
+            placeholder="Select TERTIARY healthcare factor...",
+        )
     
 
-with col2:
-    option4 = st.selectbox(
-        "4:",
-        options,
-        index=None,
-        placeholder="Select QUARTERNARY healthcare factor...",
-    )
-    option5 = st.selectbox(
-        "5:",
-        options,
-        index=None,
-        placeholder="Select QUINARY healthcare factor...",
-    )
-    option6 = st.selectbox(
-        "6:",
-        options,
-        index=None,
-        placeholder="Select SENARY healthcare factor...",
-    )
+    with col1b:
+        option4 = st.selectbox(
+            "4:",
+            options,
+            index=None,
+            placeholder="Select QUARTERNARY healthcare factor...",
+        )
+        st.write("\n")
 
-with col3:
+        option5 = st.selectbox(
+            "5:",
+            options,
+            index=None,
+            placeholder="Select QUINARY healthcare factor...",
+        )
+        st.write("\n")
+
+        option6 = st.selectbox(
+            "6:",
+            options,
+            index=None,
+            placeholder="Select SENARY healthcare factor...",
+        )
+
+with col2:
     on = st.toggle("Map / Bar Chart")
 
     if on:
-        st.write("Feature activated!")
-    
+        st.bar_chart(df)
+    else: st.map(df)
 
 
+st.write("")
+st.write("")
 
-prevention = st.slider("Prevention Score: ", 0, 100, 25)
-healthsys = st.slider("Health System Score: ", 0, 100, 25)
-rapidresp = st.slider("Rapid Response Score: ", 0, 100, 25)
-detectreport = st.slider("Detection & Reporting Score: ", 0, 100, 25)
-normscomp = st.slider("International Norms Compliance Score: ", 0, 100, 25)
-riskenv = st.slider("Risk Environment Score: ", 0, 100, 25)
+st.write("#### Use the Sliders")
+
+col1,col2 = st.columns(2)
+with col1:  
+    prevention = st.slider("Prevention Score: ", 0, 100, 25)
+    healthsys = st.slider("Health System Score: ", 0, 100, 25)
+    rapidresp = st.slider("Rapid Response Score: ", 0, 100, 25)
+
+with col2:
+    detectreport = st.slider("Detection & Reporting Score: ", 0, 100, 25)
+    normscomp = st.slider("International Norms Compliance Score: ", 0, 100, 25)
+    riskenv = st.slider("Risk Environment Score: ", 0, 100, 25)
 
 
 
