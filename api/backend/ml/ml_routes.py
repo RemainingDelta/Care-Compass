a1cdf9387cf37bf636e89e5a38e1fb63a0f7e489
Arthur <huang.arth@northeastern.edu>
2025-06-09 14:16:36 +0200

    worked on route for autoregressive model and added code that generates x and y for autoregressive model in create_xy function

--- api/backend/ml/ml_routes.py
+++ api/backend/ml/ml_routes.py
@@ -7,6 +7,8 @@ import numpy as np
 from backend.ml_models.cosine_similarity import get_similar
 from backend.ml_models.regression import dataframe
 from backend.ml_models.regression import predict
+from backend.ml_models.regression import autoreg_predict
+
 
 
 ml = Blueprint("ml", __name__)
@@ -117,6 +119,16 @@ def get_cosine_similar(chosen_country):
 def get_regression(input):
     inputs = [str(x.strip()) for x in input.split(',')]
     result = predict(dataframe(inputs[1]), inputs[0])
+    print("Country received:", inputs[0])
+
+    return jsonify(result)
+
+#model calls post to put weights in database
+# adds new regression weight from model to database
+@ml.route("/ml/get_autoregressive/<input>", methods=["GET"])
+def get_autoregressive(input):
+    inputs = [str(x.strip()) for x in input.split(',')]
+    result = autoreg_predict(dataframe(inputs[1]), inputs[0])
     print("Country received:", inputs[0])
 
     return jsonify(result)
